3e2d0d72fa2f2ed64c4478576b6be16a62bd9665
Hamit Mızrak <hamitmizrak@gmail.com>
2024-07-24 16:56:29 +0300

    Log, Live-Cycle, Toast

---
+++ README.md
@@ -0,0 +1,13 @@
+# Android Uygulamasına Hoşgeldiniz
+[GitHub](https://github.com/hamitmizrak/Offline_Android_TodoList.git)
+---
+
+## Loglama
+```sh
+verbose<debug<info<warn<error
+Log.v("tag","verbose");
+Log.d("tag","debug");
+Log.i("tag","info");
+Log.w("tag","warn");
+Log.e("tag","error");
+```
--- app/src/main/java/com/hamitmizrak/todolist/MainActivity.java
+++ app/src/main/java/com/hamitmizrak/todolist/MainActivity.java
@@ -1,17 +1,17 @@
 package com.hamitmizrak.todolist;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-//
-/**/
 public class MainActivity extends AppCompatActivity {
 
+    // ONCREATE(1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +22,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Loglama
+        Log.e("onCreate","1.alan Create(Oluşturuldu)");
+
+        // Toast
+        //Toast.makeText(this, "Anasayfaya Hoşgeldiniz", Toast.LENGTH_SHORT).show();
+        String welcome=getString(R.string.welcome);
+        Toast.makeText(this, welcome, Toast.LENGTH_LONG).show();
+    } //end onCreate
+
+    // ONSTART(2)
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart","2.alan Start(başladı)");
     }
-}
+
+    // ONRESUME(3)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume","3.alan Resume(Devam Ediyor)");
+    }
+
+    // ONPAUSE(4)
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("onPause","4.alan Pause(Mola) Verildi");
+    }
+
+    // ONSTOP(5)
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("onStop","5.alan Stop(Durduruldu)");
+    }
+
+    // ONRESTART(6)
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.w("onRestart","6.alan Restart(Tekrar Başladı)");
+    }
+
+    // ONDESTROY(7)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("onDestroy","7.alan Destroy(Öldü)");
+    }
+} //end MainActivity
